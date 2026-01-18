2016d95c414d7e536451afbd212e769f0ac5a144
prit10-ing <priteshingle01@gmail.com>
2026-01-18 16:46:00 +0530

    Data injection done

---
+++ books_recommender/entity/config_entity.py
@@ -0,0 +1,4 @@
+from collections import namedtuple
+
+DataIngestionConfig = namedtuple("DataIngestionConfig",["dataset_download_url",
+                                                    "raw_data_dir","ingested_data"])                                               
---
+++ main.py
@@ -0,0 +1,7 @@
+from books_recommender.pipeline.training_pipeline import TrainingPipeline
+
+
+
+obj  = TrainingPipeline()
+
+obj.start_training_pipeline()
